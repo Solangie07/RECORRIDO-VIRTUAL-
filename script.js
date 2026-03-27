894e014b79ae98358f610b66ca1b7bf010d87b0c
Solangie07 <solmillanbotero@gmail.com>
2026-03-27 15:17:02 +0000

    codigo inicial

---
+++ script.js
@@ -0,0 +1,29 @@
+body.home {
+  background: url('fondos/home.jpg') no-repeat center center fixed;
+  background-size: cover;
+}
+
+body.escena1 {
+  background: url('fondos/historia.jpg') no-repeat center center fixed;
+  background-size: cover;
+}
+
+body.escena2 {
+  background: url('fondos/caracteristicas.jpg') no-repeat center center fixed;
+  background-size: cover;
+}
+
+body.escena3 {
+  background: url('fondos/personalidad.jpg') no-repeat center center fixed;
+  background-size: cover;
+}
+
+body.escena4 {
+  background: url('fondos/cuidados.jpg') no-repeat center center fixed;
+  background-size: cover;
+}
+
+body.escena5 {
+  background: url('fondos/galeria.jpg') no-repeat center center fixed;
+  background-size: cover;
+}
